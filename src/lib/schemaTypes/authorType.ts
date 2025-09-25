f53b3ce66529b6c2139f90edc791ea2c1ca66260
Prince Owusu <powusu050@gmail.com>
2025-09-25 11:52:44 +0000

    update author

--- src/lib/schemaTypes/authorType.ts
+++ src/lib/schemaTypes/authorType.ts
@@ -1,7 +1,5 @@
-import {UserIcon} from '@sanity/icons'
-import {defineArrayMember, defineField, defineType} from 'sanity'
-
-
+import { UserIcon } from '@sanity/icons'
+import { defineType, defineField, defineArrayMember } from 'sanity'
 
 export const authorType = defineType({
   name: 'author',
@@ -11,36 +9,38 @@ export const authorType = defineType({
   fields: [
     defineField({
       name: 'name',
+      title: 'Name',
       type: 'string',
     }),
     defineField({
       name: 'slug',
+      title: 'Slug',
       type: 'slug',
       options: {
         source: 'name',
+        maxLength: 96,
       },
     }),
     defineField({
       name: 'image',
+      title: 'Profile Image',
       type: 'image',
       options: {
         hotspot: true,
       },
     }),
-
-  defineField({
-    name: 'bio',
-    title: 'Biography',
-    type: 'array',
-    of: [
-      defineArrayMember({
-        type: 'block',
-        styles: [{ title: 'Normal', value: 'normal' }],
-        lists: [],
-      }),
-    ],
-  }),
-
+    defineField({
+      name: 'bio',
+      title: 'Biography',
+      type: 'array',
+      of: [
+        defineArrayMember({
+          type: 'block',
+          styles: [{ title: 'Normal', value: 'normal' }],
+          lists: [],
+        }),
+      ],
+    }),
   ],
   preview: {
     select: {
