e212c1d98e8aafe78c94c369c20b19c9a2f3b5b4
Prince Owusu <powusu050@gmail.com>
2025-09-25 09:39:20 +0000

    update

--- src/lib/sanity/client.ts
+++ src/lib/sanity/client.ts
@@ -1,6 +1,24 @@
 import { createClient } from 'next-sanity'
 
-import { apiVersion, dataset, projectId } from '../../../../env'
+ const apiVersion = process.env.NEXT_PUBLIC_SANITY_API_VERSION || '2025-09-24'
+
+ const dataset = assertValue(
+  process.env.NEXT_PUBLIC_SANITY_DATASET,
+  'Missing environment variable: NEXT_PUBLIC_SANITY_DATASET'
+)
+
+ const projectId = assertValue(
+  process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
+  'Missing environment variable: NEXT_PUBLIC_SANITY_PROJECT_ID'
+)
+
+function assertValue<T>(v: T | undefined, errorMessage: string): T {
+  if (v === undefined) {
+    throw new Error(errorMessage)
+  }
+
+  return v
+}
 
 export const client = createClient({
   projectId,
--- src/lib/sanity/image.ts
+++ src/lib/sanity/image.ts
@@ -1,7 +1,25 @@
 import createImageUrlBuilder from '@sanity/image-url'
 import { SanityImageSource } from "@sanity/image-url/lib/types/types";
 
-import { dataset, projectId } from '../../../../env'
+ const apiVersion = process.env.NEXT_PUBLIC_SANITY_API_VERSION || '2025-09-24'
+
+ const dataset = assertValue(
+  process.env.NEXT_PUBLIC_SANITY_DATASET,
+  'Missing environment variable: NEXT_PUBLIC_SANITY_DATASET'
+)
+
+ const projectId = assertValue(
+  process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
+  'Missing environment variable: NEXT_PUBLIC_SANITY_PROJECT_ID'
+)
+
+function assertValue<T>(v: T | undefined, errorMessage: string): T {
+  if (v === undefined) {
+    throw new Error(errorMessage)
+  }
+
+  return v
+}
 
 // https://www.sanity.io/docs/image-url
 const builder = createImageUrlBuilder({ projectId, dataset })
