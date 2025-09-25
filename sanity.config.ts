4b68c2eba24de9beebb91694a18adf50c451219c
Prince Owusu <powusu050@gmail.com>
2025-09-25 09:43:58 +0000

    new updates

--- sanity.config.ts
+++ sanity.config.ts
@@ -9,7 +9,26 @@ import {defineConfig} from 'sanity'
 import {structureTool} from 'sanity/structure'
 
 // Go to https://www.sanity.io/docs/api-versioning to learn how API versioning works
-import {apiVersion, dataset, projectId} from '../env'
+ const apiVersion = process.env.NEXT_PUBLIC_SANITY_API_VERSION || '2025-09-24'
+
+ const dataset = assertValue(
+  process.env.NEXT_PUBLIC_SANITY_DATASET,
+  'Missing environment variable: NEXT_PUBLIC_SANITY_DATASET'
+)
+
+ const projectId = assertValue(
+  process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
+  'Missing environment variable: NEXT_PUBLIC_SANITY_PROJECT_ID'
+)
+
+function assertValue<T>(v: T | undefined, errorMessage: string): T {
+  if (v === undefined) {
+    throw new Error(errorMessage)
+  }
+
+  return v
+}
+
 import {schema} from './src/lib/schemaTypes'
 import {structure} from './src/lib/sanity/structure'
 
